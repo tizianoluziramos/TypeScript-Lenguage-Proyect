553ea84851ccda46f50f5465a65a6cf728116910
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2025-06-03 20:42:35 -0300

    Varias cosas...

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 // Project Credits
-
 // Main Developer:
 // Name: Tiziano Tomas Luzi Ramos
 // Email: tizianoluziramos@gmail.com
-// Github: https://github.com/tizianoluziramos
-
+// GitHub: https://github.com/tizianoluziramos
+// NPM: https://www.npmjs.com/~tizianoluziramos
 // License:
 // This project is licensed under the MIT License.
-
 // Creation Date: January 2024
 
 import * as es from './Europe/ES-ES/index.ts'
-import * as fr from './NoMoreUpdates/Europe/FR-FR/index.ts'
-import * as bn from './NoMoreUpdates/Asia/BN-BD/index.ts'
-import * as de from './NoMoreUpdates/Europe/DE-DE/index.ts'
-import * as pt from './NoMoreUpdates/Europe/PT-PT/index.ts'
-import * as zh from './NoMoreUpdates/Asia/ZH-CH/index.ts'
-import * as ru from './NoMoreUpdates/NotSyncContinent/Europe and Asia/RU-RU/index.ts'
+import * as fr from './Europe/FR-FR/index.ts'
+import * as bn from './Asia/BN-BD/index.ts'
+import * as ru from './Europe/RU-RU/index.ts'
+import * as de from './Europe/DE-DE/index.ts'
+import * as pt from './Europe/PT-PT/index.ts'
+import * as zh from './Asia/ZH-CH/index.ts'
 
 export {
     es,
